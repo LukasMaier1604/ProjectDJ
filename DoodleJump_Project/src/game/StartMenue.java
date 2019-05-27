a2e788f4b1c412af6d73d783e00f4c83a1d68ad9
SoenkeHinz <50234441+SoenkeHinz@users.noreply.github.com>
2019-05-27 15:43:45 +0200

    jetzt mit spiel

--- DoodleJump_Project/src/game/StartMenue.java
+++ DoodleJump_Project/src/game/StartMenue.java
@@ -28,16 +28,17 @@ public class StartMenue extends JFrame {
 	private DoodleAPP app;
 	private JPanel contentPane;
 	private JButton start, score, button_2;
+	private JTextField txtHallo;
 	
 	
 	public StartMenue(DoodleAPP app) {
 		setTitle("DoodleJump Startmen\u00FC\r\n");
-		setBounds(10, 10, 450, 250);
+		setBounds(10, 10, 286, 243);
 		getContentPane().setBackground(Color.WHITE);
 		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		
 		Panel panel = new Panel();
-		panel.setEnabled(false);
+		panel.setEnabled(true);
 		panel.setBackground(Color.PINK);
 		getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(5, 5));
@@ -51,19 +52,31 @@ public class StartMenue extends JFrame {
 		button_2 = new JButton("Ja Moin");
 		panel.add(button_2, BorderLayout.WEST);
 		
+		txtHallo = new JTextField();
+		txtHallo.setText("nein");
+		panel.add(txtHallo, BorderLayout.SOUTH);
+		txtHallo.setColumns(10);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		
-		this.app = app;
+		//this.app = app;
 		this.gameStart();
 	}
 	
 	public void gameStart() {
 		
-		start.addActionListener(new ActionListener() {
+		score.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 			
-				System.out.println("hallo");
+				try {
+					app = new DoodleAPP();
+					app.setVisible(true);
+				}catch(Exception o) {
+					o.printStackTrace();
+			}
+	
 			}
 			
 		});
